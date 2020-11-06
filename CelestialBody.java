ed9c4d47c79939235ea86ab43e7497fd0dfa79e5
marcogaldi <marco.galdi.h@gmail.com>
2020-11-06 05:11:59 +0100

    initial commit

---
+++ CelestialBody.java
@@ -0,0 +1,153 @@
+import java.awt.*;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class CelestialBody {
+
+    public static final double G = 6.673e-11;
+
+    private String name;
+    private double mass;
+    private int xCoord, yCoord;
+    private double xVel, yVel;
+    private double xAcc, yAcc;
+    private int size;
+    private Color color;
+
+    public CelestialBody() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public CelestialBody setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public CelestialBody setMass(double mass) {
+        this.mass = mass;
+        return this;
+    }
+
+    public int getXCoord() {
+        return xCoord;
+    }
+
+    public CelestialBody setXCoord(int xCoord) {
+        this.xCoord = xCoord;
+        return this;
+    }
+
+    public int getYCoord() {
+        return yCoord;
+    }
+
+    public CelestialBody setYCoord(int yCoord) {
+        this.yCoord = yCoord;
+        return this;
+    }
+
+    public double getXVel() {
+        return xVel;
+    }
+
+    public CelestialBody setXVel(double xVel) {
+        this.xVel = xVel;
+        return this;
+    }
+
+    public double getYVel() {
+        return yVel;
+    }
+
+    public CelestialBody setYVel(double yVel) {
+        this.yVel = yVel;
+        return this;
+    }
+
+    public double getXAcc() {
+        return xAcc;
+    }
+
+    public CelestialBody setXAcc(double xAcc) {
+        this.xAcc = xAcc;
+        return this;
+    }
+
+    public double getYAcc() {
+        return yAcc;
+    }
+
+    public CelestialBody setYAcc(double yAcc) {
+        this.yAcc = yAcc;
+        return this;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public CelestialBody setSize(int size) {
+        this.size = size;
+        return this;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public CelestialBody setColor(Color color) {
+        this.color = color;
+        return this;
+    }
+    public double distanceTo(CelestialBody body) {
+        double xDist = this.xCoord - body.xCoord;
+        double yDist = this.yCoord - body.yCoord;
+        return Math.sqrt((xDist * xDist) + (yDist * yDist));
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", CelestialBody.class.getSimpleName() + "{", "}")
+                .add("name=" + this.name)
+                .add("mass=" + this.mass)
+                .add("xCoord=" + this.xCoord)
+                .add("yCoord=" + this.yCoord)
+                .add("xDir=" + this.xVel)
+                .add("yDir=" + this.yVel)
+                .add("xAcc=" + this.xAcc)
+                .add("yAcc=" + this.yAcc)
+                .add("size=" + this.size)
+                .add("color=" + this.color)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (!(other instanceof CelestialBody))
+            return false;
+        CelestialBody celestialBody = (CelestialBody) other;
+        return  Objects.equals(celestialBody.name, this.name) &&
+                celestialBody.mass == this.mass &&
+                celestialBody.xCoord == this.xCoord &&
+                celestialBody.yCoord == this.yCoord &&
+                celestialBody.xVel == this.xVel &&
+                celestialBody.yVel == this.yVel &&
+                celestialBody.xAcc == this.xAcc &&
+                celestialBody.yAcc == this.yAcc &&
+                celestialBody.size == this.size &&
+                Objects.equals(celestialBody.color, this.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, mass, xCoord, yCoord, xVel, yVel, xAcc, yAcc, size, color);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,143 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class Main  {
+
+    private static final String DATA_FILE = "/resources/bodies.dat";
+
+    private static final int DEFAULT_WIDTH = 768;
+    private static final int DEFAULT_HEIGHT = 768;
+
+    private static List<CelestialBody> bodies;
+    private static double scale;    // the distance in meters represented by one pixel
+
+    // GUI elements
+    private static JFrame frame;
+
+    static {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException |
+                IllegalAccessException | UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(frame, e.getMessage(), null, JOptionPane.ERROR_MESSAGE);
+            System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Main.loadData(Main.class.getResource(DATA_FILE));
+            Main.initGui();
+            Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+                Main.updateAcceleration(bodies);
+                Main.updatePosition(bodies);
+                Main.updateVelocity(bodies);
+                frame.repaint();
+            }, 0, 15, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame, e.getMessage(), null, JOptionPane.ERROR_MESSAGE);
+            System.exit(-1);
+        }
+    }
+
+    // @todo
+    private static void loadData(URL resource) throws IOException, NumberFormatException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(resource.getFile()))) {
+            switch (reader.readLine().toLowerCase()) {
+                case "arraylist":
+                    bodies = new ArrayList<>();
+                    break;
+                case "linkedlist":
+                    bodies = new LinkedList<>();
+                    break;
+                default:
+                    // @fixme
+                    break;
+            }
+            scale = Double.parseDouble(reader.readLine());
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String[] bodyValues = line.split(",");
+                bodies.add(new CelestialBody()
+                        .setName(bodyValues[0])
+                        .setMass(Double.parseDouble(bodyValues[1]))
+                        .setXCoord(Integer.parseInt(bodyValues[2]))
+                        .setYCoord(Integer.parseInt(bodyValues[3]))
+                        .setXVel(Double.parseDouble(bodyValues[4]))
+                        .setYVel(Double.parseDouble(bodyValues[5]))
+                        .setSize(Integer.parseInt(bodyValues[6]))
+                        .setColor(new Color(ThreadLocalRandom.current().nextInt(0, 256),
+                                        ThreadLocalRandom.current().nextInt(0, 256),
+                                        ThreadLocalRandom.current().nextInt(0, 256)).brighter()));
+            }
+        }
+    }
+
+    private static void initGui() {
+        JPanel panel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics graphics) {
+                super.paintComponent(graphics);
+                bodies.forEach(body -> Main.drawCelestialBody(body, graphics));
+            }
+        };
+        panel.setBackground(new Color(33, 33, 33));
+
+        frame = new JFrame();
+        frame.add(panel);
+        frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+        private static void updateAcceleration(List<CelestialBody> bodies) {
+
+
+            for (CelestialBody toUpdate : bodies) {
+                toUpdate.setXAcc(0);
+                toUpdate.setYAcc(0);
+                for (CelestialBody body: bodies){
+                    if (!toUpdate.equals(body)) {
+                        System.out.println("to update " + toUpdate.getName() + " body " + body.getName());
+                        double force = (CelestialBody.G * ( toUpdate.getMass() * body.getMass() / Math.pow(
+                                toUpdate.distanceTo(body), 2)));
+                        double forceX = force * ((body.getXCoord() - toUpdate.getXCoord()));///toUpdate.distanceTo(body));
+                        double forceY = force * ((body.getYCoord() - toUpdate.getYCoord()));///toUpdate.distanceTo(body));
+                        double accX = (forceX/toUpdate.getMass()) + toUpdate.getXAcc();
+                        double accY = (forceY/toUpdate.getMass()) + toUpdate.getYAcc();
+                        toUpdate.setXAcc(accX);
+                        toUpdate.setYAcc(accY);
+                    }
+                }
+            }
+        }
+        private static void updatePosition(List<CelestialBody> bodies) {
+            for (CelestialBody toUpdate : bodies) {
+                toUpdate.setXCoord((int)(toUpdate.getXCoord() + toUpdate.getXAcc() + toUpdate.getXVel()));
+                toUpdate.setYCoord((int)(toUpdate.getYCoord() + toUpdate.getYAcc() + toUpdate.getYVel()));
+            }
+        }
+
+        private static void updateVelocity(List<CelestialBody> bodies) {
+            for (CelestialBody toUpdate : bodies) {
+                toUpdate.setXVel(toUpdate.getXVel() + toUpdate.getXAcc());
+                toUpdate.setYVel(toUpdate.getYVel() + toUpdate.getYAcc());
+
+            }
+    }
+
+    private static void drawCelestialBody(CelestialBody body, Graphics g) {
+        g.setColor(body.getColor());
+        g.fillOval(body.getXCoord(), body.getYCoord(), body.getSize(), body.getSize());
+    }
+}
